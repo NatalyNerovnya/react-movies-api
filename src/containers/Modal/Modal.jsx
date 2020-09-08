3dfec5131f5823008bb9356f50c6adc70e78854c
Nataly Nerovnya <natanerovnja@gmail.com>
2020-09-08 15:41:33 +0300

    rewrite Modal to functional component

--- src/containers/Modal/Modal.jsx
+++ src/containers/Modal/Modal.jsx
@@ -1,41 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default class Modal extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {isActive: false};
-        this.close = this.close.bind(this);
-        this.open = this.open.bind(this);
-
-    }
-
-    close(e){
+const Modal = (props) => {
+    const close = (e) => {
         e.preventDefault();
-        this.setState({isActive: false});
-        this.props.close(e);
+        props.close(e);
     }
 
-    open(e){
+    const open = (e) => {
         e.preventDefault();
-        this.setState({isActive: true});
-        this.props.open(e);
+        props.open(e);
     }
 
-    render(){
     return <>
-    <div onClick={(e) => this.open(e)} className={this.props.buttonClass + " modal-button"}>{this.props.buttonText}</div>
-    {this.props.isActive ? 
+    <div onClick={(e) => open(e)} className={props.buttonClass + " modal-button"}>{props.buttonText}</div>
+    {props.isActive ? 
         <div className="modal">
         <div className="modal-content">
-          <a className="close" onClick={(e) => this.close(e)}>&times;</a>
-          <h2 className="modal-label">{this.props.label}</h2>
-          {this.props.children}
+          <a className="close" onClick={(e) => close(e)}>&times;</a>
+          <h2 className="modal-label">{props.label}</h2>
+          {props.children}
         </div>
       </div>
          : ''}
     </>
-    }
 }
 
 Modal.propTypes = {
@@ -45,3 +33,5 @@ Modal.propTypes = {
     open: PropTypes.func,
     label: PropTypes.string
 }
+
+export default Modal;
